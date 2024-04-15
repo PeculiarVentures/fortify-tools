eb43cd96e87d07614350a631d5d1a8673c917833
alex-slobodian <alexx.slobodyan@gmail.com>
2024-04-15 18:27:58 +0300

    Update vite to latest

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { ThemeProvider } from '@peculiar/react-components';
-import { App } from './app';
+import ReactDOM from "react-dom";
+import { ThemeProvider } from "@peculiar/react-components";
+import { App } from "./app";
 
 ReactDOM.render(
   <ThemeProvider>
     <App />
   </ThemeProvider>,
-  document.getElementById('root'),
+  document.getElementById("root"),
 );
--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -7,5 +7,8 @@ export default defineConfig({
   server: {
     port: 3000,
     open: true,
+  },
+  build: {
+    outDir: "./build",
   },
 });
