046c85f8cdee83cc46aa647dd47815339976b979
alex-slobodian <alexx.slobodyan@gmail.com>
2024-04-17 20:14:23 +0300

    Add theme colors

--- src/components/providers/Providers.tsx
+++ src/components/providers/Providers.tsx
@@ -1,8 +1,13 @@
 import React from "react";
 import { ThemeProvider } from "@peculiar/react-components";
+import { theme } from "../../config/theme";
 
 export const Providers: React.FunctionComponent<{
   children: React.ReactElement;
 }> = ({ children }) => {
-  return <ThemeProvider>{children}</ThemeProvider>;
+  return (
+    <ThemeProvider theme={theme} cssVarsRoot="#root">
+      {children}
+    </ThemeProvider>
+  );
 };
