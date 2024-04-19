feb14ea5c3f51b786edba991c3adba32abf8561a
alex-slobodian <alexx.slobodyan@gmail.com>
2024-04-19 15:12:48 +0300

    Improve app sidebar

--- src/components/certificates-sidebar/CertificatesSidebar.tsx
+++ src/components/certificates-sidebar/CertificatesSidebar.tsx
@@ -2,6 +2,8 @@ import clsx from "clsx";
 import React, { ComponentProps } from "react";
 
 import styles from "./styles/index.module.scss";
+import { useTranslation } from "react-i18next";
+import { APP_CONTACT_SUPPORT_URL } from "../../config";
 
 interface CertificatesSidebarProps {
   className?: ComponentProps<"div">["className"];
@@ -9,11 +11,24 @@ interface CertificatesSidebarProps {
 export const CertificatesSidebar: React.FunctionComponent<
   CertificatesSidebarProps
 > = ({ children, className }) => {
+  const { t } = useTranslation();
+
   return (
     <div className={clsx(styles.sidebar_root, className)}>
-      <div>Logo</div>
+      <div className={styles.header}>
+        <img
+          className={styles.logo}
+          src="/images/logo.svg"
+          alt={t("sidebar.logo-alt")}
+        />
+      </div>
       <div className={styles.content}>{children}</div>
-      <div>Footer</div>
+      <div className={styles.footer}>
+        <div>{t("sidebar.copyrigth")}</div>
+        <div>
+          <a href={APP_CONTACT_SUPPORT_URL}>{t("sidebar.contact-support")}</a>
+        </div>
+      </div>
     </div>
   );
 };
---
+++ src/config/index.ts
@@ -0,0 +1,2 @@
+//TODO: change to correct
+export const APP_CONTACT_SUPPORT_URL = "mailto:info@fortifyapp.com";
