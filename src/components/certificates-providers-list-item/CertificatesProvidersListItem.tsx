02b8f5ca810d0a3b82be06dc44ca5cb499aceb83
alex-slobodian <alexx.slobodyan@gmail.com>
2024-04-19 21:34:53 +0300

    Style providers list component

--- src/components/certificates-providers-list-item/CertificatesProvidersListItem.tsx
+++ src/components/certificates-providers-list-item/CertificatesProvidersListItem.tsx
@@ -2,6 +2,8 @@ import React from "react";
 import { IProviderInfo } from "@peculiar/fortify-client-core";
 import styles from "./styles/index.module.scss";
 import clsx from "clsx";
+import { useTranslation } from "react-i18next";
+import ProviderIcon from "../../icons/provider.svg?react";
 
 interface CertificatesProvidersListItemProps {
   provider: IProviderInfo;
@@ -12,16 +14,27 @@ interface CertificatesProvidersListItemProps {
 export const CertificatesProvidersListItem: React.FunctionComponent<
   CertificatesProvidersListItemProps
 > = ({ provider, isSelected, onClick }) => {
+  const { t } = useTranslation();
   return (
     <div
-      className={clsx({
+      className={clsx(styles.list_item, {
         [styles.current_list_item]: isSelected,
       })}
       onClick={() => {
         onClick && onClick(provider.id);
       }}
     >
-      {provider.name}
+      <div className={styles.list_item_icon_wrapper}>
+        <ProviderIcon />
+      </div>
+      <div className={styles.list_item_name_wrapper}>
+        <div className={styles.list_item_name}>{provider.name}</div>
+        {!provider.readOnly ? (
+          <div className={styles.list_item_read_only}>
+            {t("providers.list.item.read-only")}
+          </div>
+        ) : null}
+      </div>
     </div>
   );
 };
--- src/components/certificates-providers-list/CertificatesProvidersList.tsx
+++ src/components/certificates-providers-list/CertificatesProvidersList.tsx
@@ -1,6 +1,9 @@
 import React from "react";
 import { IProviderInfo } from "@peculiar/fortify-client-core";
 import { CertificatesProvidersListItem } from "../certificates-providers-list-item";
+import { useTranslation } from "react-i18next";
+
+import styles from "./styles/index.module.scss";
 
 interface CertificatesProvidersListProps {
   providers?: IProviderInfo[];
@@ -11,23 +14,27 @@ interface CertificatesProvidersListProps {
 export const CertificatesProvidersList: React.FunctionComponent<
   CertificatesProvidersListProps
 > = ({ providers, currentProviderId, onSelect }) => {
+  const { t } = useTranslation();
   if (!providers?.length) {
     //TODO: return some message
     return null;
   }
 
   return (
-    <div>
-      {providers.map((provider) => (
-        <CertificatesProvidersListItem
-          isSelected={currentProviderId === provider.id}
-          onClick={(id) => {
-            onSelect && onSelect(id);
-          }}
-          key={provider.id}
-          provider={provider}
-        />
-      ))}
+    <div className={styles.list_wrapper}>
+      <div className={styles.label}>{t("providers.list.label")}</div>
+      <div className={styles.list}>
+        {providers.map((provider) => (
+          <CertificatesProvidersListItem
+            isSelected={currentProviderId === provider.id}
+            onClick={(id) => {
+              onSelect && onSelect(id);
+            }}
+            key={provider.id}
+            provider={provider}
+          />
+        ))}
+      </div>
     </div>
   );
 };
