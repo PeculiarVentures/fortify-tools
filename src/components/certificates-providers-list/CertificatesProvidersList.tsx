caa0793c0bf037ca7a16df2faef3a29776eb80d9
alex-slobodian <alexx.slobodyan@gmail.com>
2024-04-23 17:07:23 +0300

    Add empty state to CertificatesProvidersList

--- src/components/certificates-providers-list/CertificatesProvidersList.tsx
+++ src/components/certificates-providers-list/CertificatesProvidersList.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { IProviderInfo } from "@peculiar/fortify-client-core";
 import { CertificatesProvidersListItem } from "../certificates-providers-list-item";
 import { useTranslation } from "react-i18next";
+import AttentionCircleIcon from "../../icons/attention-circle.svg?react";
 
 import styles from "./styles/index.module.scss";
 
@@ -15,25 +16,32 @@ export const CertificatesProvidersList: React.FunctionComponent<
   CertificatesProvidersListProps
 > = ({ providers, currentProviderId, onSelect }) => {
   const { t } = useTranslation();
-  if (!providers?.length) {
-    // TODO: return some message
-    return null;
-  }
 
   return (
     <div className={styles.list_wrapper}>
       <div className={styles.label}>{t("providers.list.label")}</div>
       <div className={styles.list}>
-        {providers.map((provider) => (
-          <CertificatesProvidersListItem
-            isSelected={currentProviderId === provider.id}
-            onClick={(id) => {
-              onSelect && onSelect(id);
-            }}
-            key={provider.id}
-            provider={provider}
-          />
-        ))}
+        {!providers?.length ? (
+          <div className={styles.empty_list}>
+            <div className={styles.empty_list_icon}>
+              <AttentionCircleIcon />
+            </div>
+            <div className={styles.empty_list_text}>
+              {t("providers.list.empty-text")}
+            </div>
+          </div>
+        ) : (
+          providers.map((provider) => (
+            <CertificatesProvidersListItem
+              isSelected={currentProviderId === provider.id}
+              onClick={(id) => {
+                onSelect && onSelect(id);
+              }}
+              key={provider.id}
+              provider={provider}
+            />
+          ))
+        )}
       </div>
     </div>
   );
