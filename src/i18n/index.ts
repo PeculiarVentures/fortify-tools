cc287f40da53ff7c87661a936788fa02bf19aa33
alex-slobodian <alexx.slobodyan@gmail.com>
2024-04-23 17:47:55 +0300

    Remove i18next-icu

--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -1,6 +1,5 @@
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
-import ICU from "i18next-icu";
 
 import enLocale from "./locales/en/main.json";
 
@@ -12,19 +11,16 @@ const resources = {
   },
 };
 
-i18n
-  .use(ICU)
-  .use(initReactI18next)
-  .init({
-    resources,
-    load: "languageOnly",
-    fallbackLng: supportedLngs[0],
-    ns: ["main"],
-    supportedLngs: supportedLngs,
-    debug: import.meta.env.MODE === "development",
-    interpolation: {
-      escapeValue: false,
-    },
-  });
+i18n.use(initReactI18next).init({
+  resources,
+  load: "languageOnly",
+  fallbackLng: supportedLngs[0],
+  ns: ["main"],
+  supportedLngs: supportedLngs,
+  debug: import.meta.env.MODE === "development",
+  interpolation: {
+    escapeValue: false,
+  },
+});
 
 export default i18n;
