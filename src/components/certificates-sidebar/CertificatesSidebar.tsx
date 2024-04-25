73bd42225f470fd09e1684e9a55e22640032128e
alex-slobodian <alexx.slobodyan@gmail.com>
2024-04-25 14:58:11 +0300

    Fix sidebar footer labels

--- src/components/certificates-sidebar/CertificatesSidebar.tsx
+++ src/components/certificates-sidebar/CertificatesSidebar.tsx
@@ -1,9 +1,9 @@
 import clsx from "clsx";
 import React, { ComponentProps } from "react";
-
-import styles from "./styles/index.module.scss";
 import { useTranslation } from "react-i18next";
+import { Typography } from "@peculiar/react-components";
 import { APP_CONTACT_SUPPORT_URL } from "../../config";
+import styles from "./styles/index.module.scss";
 
 interface CertificatesSidebarProps {
   className?: ComponentProps<"div">["className"];
@@ -25,9 +25,18 @@ export const CertificatesSidebar: React.FunctionComponent<
       </div>
       <div className={styles.content}>{children}</div>
       <div className={styles.footer}>
-        <div>{t("sidebar.copyrigth")}</div>
+        <Typography variant="c1" color="gray-9">
+          {t("sidebar.copyrigth")}
+        </Typography>
         <div>
-          <a href={APP_CONTACT_SUPPORT_URL}>{t("sidebar.contact-support")}</a>
+          <Typography
+            variant="c1"
+            color="primary"
+            component="a"
+            href={APP_CONTACT_SUPPORT_URL}
+          >
+            {t("sidebar.contact-support")}
+          </Typography>
         </div>
       </div>
     </div>
