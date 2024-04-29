fc330cdf5f17a59cf28936c0f7daa4c087c9b0aa
Oleksandr Slobodian <129727832+OleksandrSPV@users.noreply.github.com>
2024-04-29 18:51:04 +0300

    Add storybook (#121)
    Co-authored-by: alex-slobodian <alexx.slobodyan@gmail.com>

---
+++ .storybook/preview.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import type { Preview } from "@storybook/react";
+import { ThemeProvider, ToastProvider } from "@peculiar/react-components";
+import { theme } from "../src/config/theme";
+import i18n from "../src/i18n";
+import "../src/global.scss";
+import "./storybook.css";
+
+const preview: Preview = {
+  parameters: {
+    i18n,
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/i,
+      },
+    },
+  },
+  decorators: [
+    (Story) => (
+      <ThemeProvider theme={theme}>
+        <ToastProvider>
+          <Story />
+        </ToastProvider>
+      </ThemeProvider>
+    ),
+  ],
+};
+
+export default preview;
