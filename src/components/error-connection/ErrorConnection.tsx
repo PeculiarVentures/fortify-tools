96656b7943267dcf6bf58b7cd5fe4e873d935be1
alex-slobodian <alexx.slobodyan@gmail.com>
2024-04-30 16:38:56 +0300

    Add styles to FetchingStatusOwerlay

--- src/components/error-connection/ErrorConnection.tsx
+++ src/components/error-connection/ErrorConnection.tsx
@@ -1,4 +1,8 @@
 import React from "react";
+import { Typography } from "@peculiar/react-components";
+import ErrorIcon from "../../icons/error-big.svg?react";
+
+import styles from "./styles/index.module.scss";
 
 interface ErrorConnectionProps {
   message: string;
@@ -10,9 +14,18 @@ export const ErrorConnection: React.FunctionComponent<ErrorConnectionProps> = (
   const { message, description } = props;
 
   return (
-    <>
-      <h1>{message}</h1>
-      {description ? <p>{description}</p> : null}
-    </>
+    <div className={styles.box_wrapper}>
+      <div className={styles.icon_wrapper}>
+        <ErrorIcon />
+      </div>
+      <Typography variant="h5" color="black">
+        {message}
+      </Typography>
+      {description ? (
+        <Typography variant="b2" color="gray-9">
+          {description}
+        </Typography>
+      ) : null}
+    </div>
   );
 };
