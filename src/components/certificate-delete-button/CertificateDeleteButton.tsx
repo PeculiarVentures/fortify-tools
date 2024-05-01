fa01f9d6d56f9ea71c4293bbe0e70ad7c7c7bd7e
alex-slobodian <alexx.slobodyan@gmail.com>
2024-05-01 13:37:47 +0300

    Create CertificateDeleteButton component

---
+++ src/components/certificate-delete-button/CertificateDeleteButton.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { useTranslation } from "react-i18next";
+import { IconButton, Tooltip } from "@peculiar/react-components";
+
+import DeleteIcon from "../../icons/delete.svg?react";
+
+interface CertificateDeleteButtonProps {
+  onClick?: () => void;
+}
+
+export const CertificateDeleteButton: React.FunctionComponent<
+  CertificateDeleteButtonProps
+> = (props) => {
+  const { onClick } = props;
+  const { t } = useTranslation();
+
+  return (
+    <Tooltip title={t("certificates.list.action.delete")} offset={5}>
+      <IconButton onClick={onClick} size="small">
+        <DeleteIcon />
+      </IconButton>
+    </Tooltip>
+  );
+};
