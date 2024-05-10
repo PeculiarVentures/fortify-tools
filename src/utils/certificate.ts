ad8e01d8632626bac13bd3e3d4002286fa893378
alex-slobodian <alexx.slobodyan@gmail.com>
2024-05-10 14:40:31 +0300

    Fix naming

--- src/utils/certificate.ts
+++ src/utils/certificate.ts
@@ -1,15 +1,16 @@
+import { Convert } from "pvtsutils";
 import { Pkcs10CertificateRequest, X509Certificate } from "@peculiar/x509";
 
-export function copyCertificate(certBase64: string, type: "x509" | "csr") {
+export function certificateRawToPem(raw: ArrayBuffer, type: "x509" | "csr") {
   let pem;
   switch (type) {
     case "x509": {
-      const cert = new X509Certificate(certBase64);
+      const cert = new X509Certificate(Convert.ToBase64(raw));
       pem = cert.toString("pem");
       break;
     }
     case "csr": {
-      const req = new Pkcs10CertificateRequest(certBase64);
+      const req = new Pkcs10CertificateRequest(Convert.ToBase64(raw));
       pem = req.toString("pem");
       break;
     }
