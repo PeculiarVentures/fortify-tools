2ab6f44d1efd24f1329ef2b759d643001cc1f728
alex-slobodian <alexx.slobodyan@gmail.com>
2024-05-10 18:34:48 +0300

    Add vitest

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,20 @@
-import { defineConfig } from "vite";
+import { defineConfig, mergeConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import tsconfigPaths from "vite-tsconfig-paths";
 import svgr from "vite-plugin-svgr";
+import viteConfig from "./vite.config.ts";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [svgr(), react()],
-  server: {
-    port: 3000,
-    open: true,
-  },
-  build: {
-    outDir: "./dist",
-  },
-});
+export default mergeConfig(
+  defineConfig({
+    plugins: [svgr(), react(), tsconfigPaths()],
+    server: {
+      port: 3000,
+      open: true,
+    },
+    build: {
+      outDir: "./dist",
+    },
+  }),
+  viteConfig
+);
