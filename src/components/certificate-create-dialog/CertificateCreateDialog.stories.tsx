2f472aeae843918d39ad1ffd0e3bf3463619693d
alex-slobodian <alexx.slobodyan@gmail.com>
2024-05-14 16:05:04 +0300

    Add CertificateCreateDialog layout

---
+++ src/components/certificate-create-dialog/CertificateCreateDialog.stories.tsx
@@ -0,0 +1,38 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import { fn } from "@storybook/test";
+import { CertificateCreateDialog } from "./CertificateCreateDialog";
+
+const meta: Meta<typeof CertificateCreateDialog> = {
+  title: "Components/CertificateCreateDialog",
+  component: CertificateCreateDialog,
+};
+
+export default meta;
+type Story = StoryObj<typeof CertificateCreateDialog>;
+
+const providers = [
+  {
+    id: "1",
+    name: "Provider 1",
+  },
+  {
+    id: "2",
+    name: "Provider 2",
+  },
+];
+
+export const Default: Story = {
+  args: {
+    currentProviderId: "2",
+    providers,
+    onProviderSelect: fn(),
+  },
+};
+
+export const isLoading: Story = {
+  args: {
+    loading: true,
+    providers,
+    onProviderSelect: fn(),
+  },
+};
---
+++ src/components/certificate-create-dialog/CertificateCreateDialog.tsx
@@ -0,0 +1,88 @@
+import React from "react";
+import { useTranslation } from "react-i18next";
+import { IProviderInfo } from "@peculiar/fortify-client-core";
+import clsx from "clsx";
+import {
+  Dialog,
+  ArrowRightIcon,
+  IconButton,
+  Typography,
+  CircularProgress,
+} from "@peculiar/react-components";
+
+import styles from "./styles/index.module.scss";
+
+interface CertificateCreateDialogProps {
+  type: "x509" | "csr";
+  currentProviderId?: string;
+  providers: Pick<IProviderInfo, "id" | "name">[];
+  loading?: boolean;
+  onProviderSelect: (id: string) => void;
+  onDialogClose: () => void;
+  onCreateClick: (certificate: string) => void;
+}
+
+export const CertificateCreateDialog: React.FunctionComponent<
+  CertificateCreateDialogProps
+> = (props) => {
+  const {
+    loading,
+    type,
+    // providers,
+    // currentProviderId,
+    // onProviderSelect,
+    onDialogClose,
+    // onCreateClick,
+  } = props;
+
+  const { t } = useTranslation();
+
+  return (
+    <Dialog open fullScreen className={styles.dialog} onClose={onDialogClose}>
+      <>
+        <div className={styles.title}>
+          <div className={styles.centered}>
+            <div>
+              <IconButton
+                onClick={onDialogClose}
+                className={styles.button_back}
+                size="small"
+              >
+                <ArrowRightIcon className={styles.arrow_back} />
+              </IconButton>
+            </div>
+            <div className={styles.title_label}>
+              <Typography variant="h4" color="black">
+                {t(
+                  type === "csr"
+                    ? "certificates.dialog.create.title.csr"
+                    : "certificates.dialog.create.title.x509"
+                )}
+              </Typography>
+            </div>
+            <div>
+              {
+                // CertificatesProvidersSelectList
+              }
+            </div>
+          </div>
+        </div>
+        <div className={styles.content}>
+          <div className={clsx(styles.centered, styles.content_box)}>
+            {
+              // content
+            }
+          </div>
+        </div>
+        {loading ? (
+          <div className={styles.loading}>
+            <CircularProgress />
+            <Typography variant="b2" color="gray-9">
+              {t("certificates.dialog.create.loading-text")}
+            </Typography>
+          </div>
+        ) : null}
+      </>
+    </Dialog>
+  );
+};
