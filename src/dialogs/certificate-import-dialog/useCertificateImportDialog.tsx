c970d5afd061fe1b70f11efafc6468ce00217cf4
Oleksandr Slobodian <129727832+OleksandrSPV@users.noreply.github.com>
2024-05-20 11:36:17 +0300

    Version 2: Add import certificate dialog (#132)
    * Add CertificatesProvidersSelectList component
    
    * Add CertificateImportDialog component
    
    * Certificate Import Dialog open/close
    
    * Add file drop zone
    
    * Styles drop zone
    
    * Certificate processing
    
    * Fix certificate processing
    
    * Add actions
    
    * Fix title border
    
    * Fix styles
    
    * Fix drop zone styles & behaviour
    
    * Fix styles
    
    * Fix dialog title
    
    * Fix CertificatesProvidersSelectList style
    
    * Add useCertificateImportDialog
    
    * Fix provider selection
    
    * Fix provider select popover
    
    * Fix upload certificate
    
    * Lock fix
    
    ---------
    
    Co-authored-by: alex-slobodian <alexx.slobodyan@gmail.com>

---
+++ src/dialogs/certificate-import-dialog/useCertificateImportDialog.tsx
@@ -0,0 +1,102 @@
+import React, { useRef } from "react";
+import { IProviderInfo } from "@peculiar/fortify-client-core";
+import { X509Certificate } from "@peculiar/x509";
+import { useToast } from "@peculiar/react-components";
+import { useTranslation } from "react-i18next";
+import { CertificateImportDialog } from "../../components/certificate-import-dialog";
+
+export function useCertificateImportDialog(props: {
+  providers: IProviderInfo[];
+  currentProviderId?: string;
+}) {
+  const { providers, currentProviderId } = props;
+  const { addToast } = useToast();
+  const { t } = useTranslation();
+
+  const [isOpen, setIsOpen] = React.useState(false);
+  const [isLoading, setIsLoading] = React.useState(false);
+
+  const [certificate, setCertificate] = React.useState("");
+  const [isTextAreaError, setIsTextAreaError] = React.useState(false);
+
+  const localCurrentProviderId = useRef(currentProviderId);
+
+  const handleCertificateImport = () => {
+    // TODO: add logic
+    console.log("Import", certificate);
+    console.log("localCurrentProviderId", localCurrentProviderId?.current);
+    // temporary behaviour
+    setIsLoading(true);
+    setTimeout(function () {
+      setIsLoading(false);
+      addToast({
+        message: t("certificates.dialog.import.failure-message"),
+        variant: "wrong",
+        disableIcon: true,
+        isClosable: true,
+      });
+    }, 1000);
+  };
+
+  const onDropAccepted = (fileContent: ArrayBuffer) => {
+    const x509Cert = new X509Certificate(fileContent);
+
+    setCertificate(x509Cert.toString("pem"));
+    setIsTextAreaError(false);
+  };
+
+  return {
+    open: () => setIsOpen(true),
+    dialog: () =>
+      isOpen ? (
+        <CertificateImportDialog
+          certificate={certificate}
+          isTextAreaError={isTextAreaError}
+          onTextAreaChange={(value) => {
+            setCertificate(value);
+            setIsTextAreaError(false);
+          }}
+          onTextAreaBlur={() => {
+            try {
+              // certificate?.length && new X509Certificate(certificate);
+
+              setIsTextAreaError(false);
+            } catch (error) {
+              setIsTextAreaError(true);
+            }
+          }}
+          onDropAccepted={onDropAccepted}
+          onDropError={() => {
+            addToast({
+              message: t(
+                "certificates.dialog.import.certificate.error.invalid-data"
+              ),
+              variant: "wrong",
+              disableIcon: true,
+              isClosable: true,
+            });
+          }}
+          onDropRejected={(msg) => {
+            addToast({
+              message: msg,
+              variant: "wrong",
+              disableIcon: true,
+              isClosable: true,
+            });
+          }}
+          onDialogClose={() => setIsOpen(false)}
+          onProviderSelect={(id) => {
+            localCurrentProviderId.current = id;
+          }}
+          providers={providers}
+          currentProviderId={currentProviderId}
+          onImportButtonClick={handleCertificateImport}
+          onClearButtonClick={() => {
+            setCertificate("");
+            setIsTextAreaError(false);
+          }}
+          loading={isLoading}
+        />
+      ) : null,
+  };
+}
