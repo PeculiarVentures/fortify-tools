5540986ff03c82b7a7741119166959192a8cb8b9
alex-slobodian <alexx.slobodyan@gmail.com>
2024-08-02 17:20:56 +0300

    Add test for useCertificateCreateDialog hook

---
+++ src/dialogs/certificate-create-dialog/useCertificateCreateDialog.test.tsx
@@ -0,0 +1,161 @@
+import {
+  describe,
+  it,
+  renderHook,
+  act,
+  expect,
+  render,
+  cleanup,
+  afterEach,
+} from "@testing";
+import { vi } from "vitest";
+import { useCertificateCreateDialog } from "./useCertificateCreateDialog";
+
+import type { IProviderInfo, FortifyAPI } from "@peculiar/fortify-client-core";
+
+afterEach(cleanup);
+
+vi.mock("@peculiar/react-components", async () => {
+  const actual = await vi.importActual("@peculiar/react-components");
+  return {
+    ...actual,
+    useToast: () => ({
+      addToast: vi.fn(),
+    }),
+  };
+});
+
+describe("useCertificateCreateDialog", () => {
+  const algorithm = { hash: "SHA-256", signature: "RSASSA-PKCS1-v1_5" };
+
+  const providers = [
+    {
+      id: "1",
+      name: "Provider 1",
+    },
+  ] as IProviderInfo[];
+
+  it("Should initialize", () => {
+    const { result } = renderHook(() =>
+      useCertificateCreateDialog({
+        providers,
+        onSuccess: vi.fn(),
+      })
+    );
+
+    expect(result.current.dialog).toBeInstanceOf(Function);
+    expect(result.current.open).toBeInstanceOf(Function);
+  });
+
+  it("Should open the dialog with x509 type", async () => {
+    const { result } = renderHook(() =>
+      useCertificateCreateDialog({
+        providers,
+        onSuccess: vi.fn(),
+      })
+    );
+
+    act(() => {
+      result.current.open("x509");
+    });
+
+    const DialogComponent = result.current.dialog();
+
+    expect(DialogComponent).not.toBeNull();
+
+    const { getByText } = render(DialogComponent);
+
+    expect(getByText("Create Self-signed certificate")).toBeInTheDocument();
+  });
+
+  it("Should open the dialog with csr type", async () => {
+    const { result } = renderHook(() =>
+      useCertificateCreateDialog({
+        providers,
+        onSuccess: vi.fn(),
+      })
+    );
+
+    act(() => {
+      result.current.open("csr");
+    });
+
+    const DialogComponent = result.current.dialog();
+
+    expect(DialogComponent).not.toBeNull();
+
+    const { getByText } = render(DialogComponent);
+
+    expect(
+      getByText("Create Certificate Signing Request (CSR)")
+    ).toBeInTheDocument();
+  });
+
+  it("Should call onSuccess (x509)", async () => {
+    const handleSuccess = vi.fn();
+    const mockFortifyClient: Partial<FortifyAPI> = {
+      createX509: vi.fn().mockResolvedValue({}),
+    };
+
+    const { result } = renderHook(() =>
+      useCertificateCreateDialog({
+        providers,
+        onSuccess: handleSuccess,
+        fortifyClient: mockFortifyClient as FortifyAPI,
+        currentProviderId: "1",
+      })
+    );
+
+    act(() => {
+      result.current.open("x509");
+    });
+
+    await act(async () => {
+      const DialogComponent = result.current.dialog();
+
+      DialogComponent &&
+        (await DialogComponent.props.onCreateButtonClick({
+          subject: { commonName: "example.com" },
+          type: "x509",
+          algorithm,
+        }));
+    });
+
+    expect(handleSuccess).toHaveBeenCalledWith("1");
+    expect(mockFortifyClient.createX509).toHaveBeenCalled();
+  });
+
+  it("Should call onSuccess (csr)", async () => {
+    const handleSuccess = vi.fn();
+    const mockFortifyClient: Partial<FortifyAPI> = {
+      createPKCS10: vi.fn().mockResolvedValue({}),
+    };
+
+    const { result } = renderHook(() =>
+      useCertificateCreateDialog({
+        providers,
+        onSuccess: handleSuccess,
+        fortifyClient: mockFortifyClient as FortifyAPI,
+        currentProviderId: "1",
+      })
+    );
+
+    act(() => {
+      result.current.open("csr");
+    });
+
+    await act(async () => {
+      const DialogComponent = result.current.dialog();
+
+      DialogComponent &&
+        (await DialogComponent.props.onCreateButtonClick({
+          subject: { commonName: "example.com" },
+          type: "csr",
+          algorithm,
+        }));
+    });
+
+    expect(handleSuccess).toHaveBeenCalledWith("1");
+    expect(mockFortifyClient.createPKCS10).toHaveBeenCalled();
+  });
+});
--- utils/testing/index.ts
+++ utils/testing/index.ts
@@ -1,3 +1,11 @@
 export { describe, it, expect, vi, afterEach } from "vitest";
-export { render, screen, cleanup, fireEvent } from "@testing-library/react";
+export {
+  render,
+  screen,
+  cleanup,
+  fireEvent,
+  renderHook,
+  act,
+  waitFor,
+} from "@testing-library/react";
 export { userEvent } from "@testing-library/user-event";
