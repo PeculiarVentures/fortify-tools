df1fff7338990f5edc45aa12606b177eb1294560
alex-slobodian <alexx.slobodyan@gmail.com>
2024-08-05 13:13:09 +0300

    Add test for CertificateTypeSelect component

---
+++ src/components/certificate-type-select/CertificateTypeSelect.test.tsx
@@ -0,0 +1,82 @@
+import {
+  describe,
+  it,
+  render,
+  vi,
+  expect,
+  userEvent,
+  cleanup,
+  afterEach,
+} from "@testing";
+import { CertificateTypeSelect } from "./CertificateTypeSelect";
+
+afterEach(cleanup);
+
+describe("<CertificateTypeSelect />", () => {
+  it("Should render for x509 & handel change", async () => {
+    const handleChange = vi.fn((data) => data);
+
+    const { getByRole, getAllByRole, queryByRole } = render(
+      <CertificateTypeSelect
+        onChange={handleChange}
+        className="test_combobox"
+        type="x509"
+      />
+    );
+
+    const combobox = getByRole("combobox");
+    expect(combobox).toBeInTheDocument();
+    expect(combobox).toHaveTextContent(/Select type/);
+    expect(combobox.parentElement).toHaveClass("test_combobox");
+
+    await userEvent.click(combobox);
+
+    const comboboxPopup = getByRole("presentation");
+    expect(comboboxPopup).toBeInTheDocument();
+
+    const comboboxItems = getAllByRole("option");
+    expect(comboboxItems).toHaveLength(6);
+
+    expect(comboboxItems[5]).toHaveTextContent(/Custom certificate/);
+
+    await userEvent.click(comboboxItems[5]);
+
+    expect(queryByRole("presentation")).not.toBeInTheDocument();
+
+    expect(handleChange).toBeCalledTimes(1);
+    expect(handleChange).toHaveReturnedWith({
+      label: "Custom certificate",
+      value: "custom",
+    });
+  });
+
+  it("Should render for CSR & handel change", async () => {
+    const handleChange = vi.fn((data) => data);
+
+    const { getByRole, getAllByRole, queryByRole } = render(
+      <CertificateTypeSelect onChange={handleChange} type="csr" />
+    );
+
+    const combobox = getByRole("combobox");
+    expect(combobox).toBeInTheDocument();
+
+    await userEvent.click(combobox);
+
+    const comboboxPopup = getByRole("presentation");
+    expect(comboboxPopup).toBeInTheDocument();
+
+    const comboboxItems = getAllByRole("option");
+
+    expect(comboboxItems[5]).toHaveTextContent(/Custom CSR/);
+
+    await userEvent.click(comboboxItems[5]);
+
+    expect(queryByRole("presentation")).not.toBeInTheDocument();
+
+    expect(handleChange).toBeCalledTimes(1);
+    expect(handleChange).toHaveReturnedWith({
+      label: "Custom CSR",
+      value: "custom",
+    });
+  });
+});
