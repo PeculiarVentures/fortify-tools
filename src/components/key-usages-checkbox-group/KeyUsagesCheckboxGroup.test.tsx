71516b2c7bfd5f74426b12947209c6042130739e
alex-slobodian <alexx.slobodyan@gmail.com>
2024-08-05 16:37:59 +0300

    Fix test KeyUsagesCheckboxGroup

--- src/components/key-usages-checkbox-group/KeyUsagesCheckboxGroup.test.tsx
+++ src/components/key-usages-checkbox-group/KeyUsagesCheckboxGroup.test.tsx
@@ -1,4 +1,5 @@
 import { render } from "@testing";
+import { certificateKeyUsageExtensions } from "src/config/data";
 import { KeyUsagesCheckboxGroup } from "./KeyUsagesCheckboxGroup";
 
 describe("<KeyUsagesCheckboxGroup />", () => {
@@ -7,9 +8,16 @@ describe("<KeyUsagesCheckboxGroup />", () => {
       <KeyUsagesCheckboxGroup className="test_class" />
     );
 
+    const keyUsageExtensions = Object.values(certificateKeyUsageExtensions);
+
     expect(container.children[0]).toHaveClass("test_class");
 
     const checkboxes = getAllByRole("checkbox");
-    expect(checkboxes).toHaveLength(5);
+    expect(checkboxes).toHaveLength(keyUsageExtensions.length);
+
+    keyUsageExtensions.forEach((value, index) => {
+      expect(checkboxes[index]).toHaveAttribute("name", "keyUsage");
+      expect(checkboxes[index]).toHaveAttribute("value", value);
+    });
   });
 });
