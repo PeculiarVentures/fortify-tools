15bc3bed17c8958e316419007847e2e02c278a48
alex-slobodian <alexx.slobodyan@gmail.com>
2024-09-02 15:29:58 +0300

    Change app dowload link

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,6 +1,5 @@
 export const APP_CONTACT_SUPPORT_URL = "mailto:support@fortifyapp.com";
-export const APP_FORTIFY_DOWLOAD_APP_URL =
-  "https://fortifyapp.com/#download_app";
+export const APP_FORTIFY_DOWLOAD_APP_URL = "https://fortifyapp.com";
 
 // https://www.iana.org/assignments/media-types/media-types.xhtml
 export const APP_CERTIFICATE_ALLOWED_MIMES = {
