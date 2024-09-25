16414c5f3729e7aaa32e5dd09fae3124359fe2cd
alex-slobodian <alexx.slobodyan@gmail.com>
2024-09-25 15:44:18 +0300

    Add tests for useCertificateImportDialog hook

---
+++ src/dialogs/certificate-import-dialog/useCertificateImportDialog.test.tsx
@@ -0,0 +1,203 @@
+import { renderHook, act } from "@testing";
+import { certificateCSRPem, certificateX509Pem } from "@testing/data";
+import { vi } from "vitest";
+import { useCertificateImportDialog } from "./useCertificateImportDialog";
+
+import type { IProviderInfo, FortifyAPI } from "@peculiar/fortify-client-core";
+
+const addToastMock = vi.fn();
+
+vi.mock("@peculiar/react-components", async () => {
+  const actual = await vi.importActual("@peculiar/react-components");
+  return {
+    ...actual,
+    useToast: () => ({
+      addToast: addToastMock,
+    }),
+  };
+});
+
+describe("useCertificateImportDialog", () => {
+  const providers = [
+    {
+      id: "1",
+      name: "Provider 1",
+    },
+  ] as IProviderInfo[];
+
+  const certStorageMock = {
+    importCert: vi.fn(),
+    setItem: vi.fn(),
+  };
+
+  const fortifyClientMock: Partial<FortifyAPI> = {
+    getProviderById: vi.fn().mockResolvedValue({
+      certStorage: certStorageMock,
+    }),
+  };
+
+  const defaultProps = {
+    providers,
+    onSuccess: vi.fn(),
+    fortifyClient: fortifyClientMock as FortifyAPI,
+    currentProviderId: providers[0].id,
+  };
+
+  async function testFileUploadCertificate(
+    certificate: string,
+    extension: string
+  ) {
+    const onSuccessMock = vi.fn();
+
+    const byteArray = new TextEncoder().encode(certificate);
+
+    const { result } = renderHook(() =>
+      useCertificateImportDialog({
+        ...defaultProps,
+        onSuccess: onSuccessMock,
+      })
+    );
+    act(() => {
+      result.current.open();
+    });
+
+    await act(async () => {
+      const DialogComponent = result.current.dialog();
+      if (DialogComponent) {
+        DialogComponent.props.onDropAccepted(byteArray.buffer, extension, "");
+        await DialogComponent.props.onImportButtonClick();
+      }
+    });
+
+    expect(fortifyClientMock.getProviderById).toHaveBeenCalledWith(
+      defaultProps.currentProviderId
+    );
+    expect(certStorageMock.importCert).toHaveBeenCalled();
+    expect(certStorageMock.setItem).toHaveBeenCalled();
+    expect(onSuccessMock).toHaveBeenCalledWith(defaultProps.currentProviderId);
+  }
+
+  async function testPasteCertificateBase64format(certificate: string) {
+    const onSuccessMock = vi.fn();
+
+    const { result } = renderHook(() =>
+      useCertificateImportDialog({
+        ...defaultProps,
+        onSuccess: onSuccessMock,
+      })
+    );
+
+    act(() => {
+      result.current.open();
+    });
+
+    act(() => {
+      result.current.dialog()?.props.onTextAreaChange(certificate);
+    });
+
+    await act(async () => {
+      const DialogComponent = result.current.dialog();
+      if (DialogComponent) {
+        DialogComponent.props.onTextAreaBlur();
+        await DialogComponent.props.onImportButtonClick();
+      }
+    });
+
+    expect(fortifyClientMock.getProviderById).toHaveBeenCalledWith(
+      defaultProps.currentProviderId
+    );
+    expect(certStorageMock.importCert).toHaveBeenCalled();
+    expect(certStorageMock.setItem).toHaveBeenCalled();
+    expect(onSuccessMock).toHaveBeenCalledWith(defaultProps.currentProviderId);
+  }
+
+  it("Should initialize", () => {
+    const { result } = renderHook(() =>
+      useCertificateImportDialog(defaultProps)
+    );
+
+    expect(result.current.dialog).toBeInstanceOf(Function);
+    expect(result.current.open).toBeInstanceOf(Function);
+  });
+
+  it("Should past certificate & call onSuccess (x509)", () =>
+    testPasteCertificateBase64format(certificateX509Pem));
+
+  it("Should past certificate & call onSuccess (CSR)", () =>
+    testPasteCertificateBase64format(certificateCSRPem));
+
+  it("Should upload file certificate & call onSuccess (x509)", () =>
+    testFileUploadCertificate(certificateX509Pem, "cer"));
+
+  it("Should upload file certificate & call onSuccess (CSR)", () =>
+    testFileUploadCertificate(certificateCSRPem, "csr"));
+
+  it("Should handle text area validation error", () => {
+    const { result } = renderHook(() =>
+      useCertificateImportDialog(defaultProps)
+    );
+
+    act(() => {
+      result.current.open();
+    });
+
+    act(() => {
+      result.current.dialog()?.props.onTextAreaChange("certificate");
+    });
+
+    act(() => {
+      result.current.dialog()?.props.onTextAreaBlur();
+    });
+
+    expect(result.current.dialog()?.props.isTextAreaError).toBeTruthy();
+  });
+
+  it("Should handle file rejection correctly", () => {
+    const { result } = renderHook(() =>
+      useCertificateImportDialog(defaultProps)
+    );
+
+    act(() => {
+      result.current.open();
+    });
+
+    const errorMessage = "Error message";
+    act(() => {
+      result.current.dialog()?.props.onDropRejected(errorMessage);
+    });
+
+    expect(addToastMock).toHaveBeenCalledWith(
+      expect.objectContaining({
+        message: errorMessage,
+        variant: "wrong",
+      })
+    );
+  });
+
+  it("Should handle the error when uploading an invalid certificate", async () => {
+    const byteArray = new TextEncoder().encode("certificate");
+    const { result } = renderHook(() =>
+      useCertificateImportDialog(defaultProps)
+    );
+
+    act(() => {
+      result.current.open();
+    });
+
+    await act(async () => {
+      const DialogComponent = result.current.dialog();
+      if (DialogComponent) {
+        DialogComponent.props.onDropAccepted(byteArray.buffer, "cer", "");
+        await DialogComponent.props.onImportButtonClick();
+      }
+    });
+
+    expect(addToastMock).toHaveBeenCalledWith(
+      expect.objectContaining({
+        message:
+          "Certificate is invalid. Please check your data and try again.",
+        variant: "wrong",
+      })
+    );
+  });
+});
