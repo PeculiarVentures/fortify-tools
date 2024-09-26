71da3e23e06adc61937baf650cc39316f2a5b8cf
alex-slobodian <alexx.slobodyan@gmail.com>
2024-09-26 13:16:28 +0300

    Update storybook to v.8.3.3

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -2,16 +2,23 @@ import type { StorybookConfig } from "@storybook/react-vite";
 
 const config: StorybookConfig = {
   stories: ["../src/**/*.stories.@(ts|tsx)"],
+
   addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
     "@storybook/addon-actions",
   ],
+
   framework: {
     name: "@storybook/react-vite",
     options: {},
   },
+
   docs: {},
+
+  typescript: {
+    reactDocgen: "react-docgen-typescript"
+  }
 };
 export default config;
