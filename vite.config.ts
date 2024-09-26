e4ac0317a167791aa3fc2a98804cd272600e4a83
alex-slobodian <alexx.slobodyan@gmail.com>
2024-09-26 13:43:57 +0300

    Update sass to v.1.79.3

--- vite.config.ts
+++ vite.config.ts
@@ -13,4 +13,11 @@ export default defineConfig({
   build: {
     outDir: "./dist",
   },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        api: "modern-compiler",
+      },
+    },
+  },
 });
