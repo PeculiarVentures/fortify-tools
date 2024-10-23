ab8ef370bd194acac95a4ae7010d72ba9f32ab74
alex-slobodian <alexx.slobodyan@gmail.com>
2024-10-23 18:28:39 +0300

    Add tests for ApproveConnection

---
+++ src/components/approve-connection/ApproveConnection.test.tsx
@@ -0,0 +1,28 @@
+import { render, screen } from "@testing";
+import { ApproveConnection } from "./ApproveConnection";
+
+vi.mock("../../icons/fortify.svg?react", () => ({
+  default: () => <svg data-testid="fortify_icon" />,
+}));
+
+describe("<ApproveConnection />", () => {
+  it("Should render", () => {
+    const challenge = "ABCDF";
+
+    render(<ApproveConnection challenge={challenge} />);
+
+    expect(screen.getByTestId("fortify_icon")).toBeInTheDocument();
+
+    expect(screen.getByText(/Fortify authorization/)).toBeInTheDocument();
+
+    challenge.split("").forEach((char) => {
+      expect(screen.getByText(char)).toBeInTheDocument();
+    });
+
+    expect(
+      screen.getByText(
+        /Compare this code to the one shown by your operating system for Fortify/
+      )
+    ).toBeInTheDocument();
+  });
+});
