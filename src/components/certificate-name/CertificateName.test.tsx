600f7919100f87000d0811f6471ef621dbc70caa
alex-slobodian <alexx.slobodyan@gmail.com>
2024-10-25 12:59:05 +0300

    Add tests for CertificateName component

---
+++ src/components/certificate-name/CertificateName.test.tsx
@@ -0,0 +1,26 @@
+import { render, screen } from "@testing";
+import { CertificateName } from "./CertificateName";
+
+describe("<CertificateName />", () => {
+  it("Should render", () => {
+    render(<CertificateName name="Test certificate" />);
+
+    expect(screen.getByText(/Test certificate/)).toBeInTheDocument();
+  });
+
+  it("Should render with highlighted text", () => {
+    render(<CertificateName name="Test certificate" highlight="rtifi" />);
+
+    const highlightElement = screen.getByText(/rtifi/);
+    expect(highlightElement).toBeInTheDocument();
+    expect(highlightElement.getAttribute("class")).toMatch("highlight_text");
+  });
+
+  it("Should render with className", () => {
+    render(
+      <CertificateName name="Test certificate" className="test_class_name" />
+    );
+
+    expect(screen.getByText(/Test certificate/)).toHaveClass("test_class_name");
+  });
+});
